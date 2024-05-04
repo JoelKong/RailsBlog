3cbb9f9027670fea92147fb813c78458df3f947f
Joel Kong <joelkong2001@gmail.com>
2024-05-04 23:05:45 +0800

    feat: brought over blog posts from api link

--- client/src/features/posts/Posts.tsx
+++ client/src/features/posts/Posts.tsx
@@ -1,3 +1,43 @@
+import { API_URL } from "../../constants";
+import { useState, useEffect } from "react";
+
 export default function Posts(): JSX.Element {
-  return <div>fg</div>;
+  const [posts, setPosts] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<String>("");
+
+  // Fetch posts from backend api in rails
+  async function loadPosts() {
+    try {
+      const response = await fetch(`${API_URL}/posts`);
+      if (response.ok) {
+        const json = await response.json();
+        setPosts(json);
+      } else {
+        throw response;
+      }
+    } catch (error) {
+      setError("An error occured. Please reload the page");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    loadPosts();
+  }, []);
+
+  return (
+    <div>
+      {posts.map((post: any) => {
+        return (
+          <div key={post.id} className="post-container">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
